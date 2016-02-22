ed1af72ed0ce263b153d815ffdeb67e83d955890
James Borrell <james.borrell92@gmail.com>
2016-02-22 13:04:33 +0000

    Placing orders with amounts

--- lib/order.rb
+++ lib/order.rb
@@ -9,8 +9,22 @@ class Order
   end
 
   def menu
+    menu_hash
+  end
+
+  def place_order(food, amount)
+    @current_order << [food, [amount, prices(food)]]
+  end
+
+private
+
+  def menu_hash
     restaurant = File.read('hipstercoffee.json')
     food = JSON.parse(restaurant)
     return food[0]['prices'][0]
+  end
+
+  def prices(food)
+    return menu_hash[food]
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -10,5 +10,16 @@ describe 'Order' do
   it 'Starts with an empty order' do
     expect(order.current_order).to eq([])
   end
+  describe 'Placing an order' do
+    it 'Adds item to current order' do
+      order.place_order("Cafe Latte", 2)
+      expect(order.current_order).to eq([["Cafe Latte", [2, 4.75]]])
+    end
+    it 'Can add multiple orders' do
+      order.place_order("Blueberry Muffin", 1)
+      order.place_order("Choc Mudcake", 1)
+      expect(order.current_order.length).to eq(2)
+    end
+  end
 
 end
