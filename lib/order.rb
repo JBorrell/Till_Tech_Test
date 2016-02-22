477a1cc7c4e88f40369200d9067173687c7b20f5
James Borrell <james.borrell92@gmail.com>
2016-02-22 12:32:19 +0000

    Adds current order property to Order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,6 +1,13 @@
 require 'json'
 
 class Order
+
+  attr_reader :current_order
+
+  def initialize
+    @current_order = []
+  end
+
   def menu
     restaurant = File.read('hipstercoffee.json')
     food = JSON.parse(restaurant)
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -6,5 +6,9 @@ describe 'Order' do
   it 'Returns items with the price' do
     expect(order.menu).to include("Americano" => 3.75)
   end
-  
+
+  it 'Starts with an empty order' do
+    expect(order.current_order).to eq([])
+  end
+
 end
